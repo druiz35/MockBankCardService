6135951f9dc5f3bfa02f42443fefc1c97568295f
druiz35 <escobarruizd@gmail.com>
2026-02-22 22:51:19 -0500

    fix: Changed module paths from example to druiz

--- src/main/java/com/druiz/mockbankcardservice/MockBankCardServiceApplication.java
+++ src/main/java/com/druiz/mockbankcardservice/MockBankCardServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.mockbankcardservice;
+package com.druiz.mockbankcardservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
