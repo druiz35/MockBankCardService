c7ee369656c4fd9f0c0eaa6789ca48d061564347
druiz35 <escobarruizd@gmail.com>
2026-02-22 23:32:21 -0500

    feat: Added config for DTO to Entity transformations

--- src/main/java/com/druiz/mockbankcardservice/config/CardModelConfig.java
+++ src/main/java/com/druiz/mockbankcardservice/config/CardModelConfig.java
@@ -1,4 +1,13 @@
 package com.druiz.mockbankcardservice.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class CardModelConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
